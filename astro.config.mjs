5554c957b17d69716172bd1d31f35ada16046ea3
Garkatron <masitacarg@gmail.com>
2026-05-04 11:20:30 +0100

    fix: AdminLayout.astro replaced with BaseLayout.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
-import vercel from '@astrojs/vercel/serverless';
+import vercelAdapter from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), icon()],
   output: 'server',
-  adapter: vercel(),
+  adapter: vercelAdapter(),
   vite: {
     plugins: [tailwindcss()]
   }
